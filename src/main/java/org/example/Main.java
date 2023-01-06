e8848e75527c3482a9e7087c351a54ec3e27dbb4
maxlazarewicz <juniorjavamlazarewicz@gmail.com>
2023-01-06 16:50:12 +0100

    Added comparator to sort LinkedList Ascending

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
@@ -13,15 +15,31 @@ public class Main {
         boolean loop = true;
         boolean exit = false;
 
+
         Town sydney = new Town("Sydney", 0);
         Town newcastle = new Town("Newcastle", 161);
         Town brisbane = new Town("Brisbane", 909);
         Town brisbane1 = new Town("Brisbane", 909);
+        Town adelaide = new Town("Ade", 100);
+        Town perth = new Town("perth", 777);
+
+
+
 
         places.addFirst(sydney);
         places.add(newcastle);
         places.add(brisbane);
         places.add(brisbane1);
+        places.add(adelaide);
+        places.add(perth);
+
+
+        Collections.sort(places, new Comparator<Town>() {
+            @Override
+            public int compare(Town o1, Town o2) {
+                return o1.getDistanceFromSydney() - o2.getDistanceFromSydney();
+            }
+        });
 
        interactiveProgram.program(loop, places, exit);
 
